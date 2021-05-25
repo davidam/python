6cfb9e1fd68ed05e66f8676b86d76ed120ba2fb1
root <root@academiahacker.incibe>
2021-05-25 20:52:25 +0200

    modificacion animal

--- animal.py
+++ animal.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python
 
-class animal(object):
-    def vivo(self, vivo):
-        vivo = true
-        return vivo
+class Animal(object):
+    def __init__(self):
+        self.estoyVivo = True
+
+    def vivo():
+        return self.estoyVivo 
 
 
-class gato(animal):
-    def ronronear(self, sonido):
-        sonido="rrrr"
+class Gato(Animal):
+    def __init__(self):
+        sonido = ""
+
+    def ronronear(self):
+        sonido = "rrrr"
         return sonido
-    def maullar(sonido):
-        sonido="miauuu"
-        return sonido
-    def grunir(sonido):
-        sonido="gggrrrr"
+
+    def maullar(self):
+        sonido = "miauuu"
         return sonido
 
+    def grunir(self):
+        sonido = "gggrrrr"
+        return sonido
 
-g = gato()
 
-sonido=""
-print(g.ronronear(sonido))
+g = Gato()
+print(g.ronronear())
 print(g.maullar())
 print(g.grunir())
+print(g.vivo())
