b4bf801af3efc2ee8f331e8804f4ca8eb0cb36a2
root <root@academiahacker.incibe>
2021-05-25 20:51:22 +0200

    nuevos ficheros

---
+++ fecha.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import time
+import datetime
+
+print("Fecha del dia", datetime.datetime.now().strftime("%y-%m-%d"))
+print("Hora", datetime.datetime.now().strftime("%H"))
+print("Minutos", datetime.datetime.now().strftime("%M"))
+print("Segundos", datetime.datetime.now().strftime("%S"))
---
+++ split.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+txt = "Es una prueba con split"
+x = txt.split()
+print(x)
