1dea32fbbd17e2205aba76dbe2fcf9ddd785426f
root <root@academiahacker.incibe>
2021-05-26 20:35:31 +0200

    leerFic

---
+++ leerFich.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+f = open("files/gpl-3.0.txt", "r")
+f2 = open("files/lineas1-15.txt", "w")
+f3 = open("files/lineas16-20.txt", "w")
+
+contLineas = 0
+
+for linea in f:
+    if contLineas < 16:
+        f2.write(linea)
+    elif contLineas < 20:
+        f3.write(linea)
+    contLineas = contLineas + 1
+
+
+f.close()
+f2.close()
+f3.close()
